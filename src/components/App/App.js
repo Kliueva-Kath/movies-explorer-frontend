d2f1ddfda9b36ab0bbde53cff154cb7af52401d6
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-10-18 04:30:18 +0300

    feat: add routes

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import { Switch, Route, useHistory } from "react-router-dom";
+import Header from "../Header/Header.js";
+import Main from "../Main/Main.js";
+import Footer from "../Footer/Footer.js";
+import Movies from "../Movies/Movies.js";
+import SavedMovies from "../SavedMovies/SavedMovies.js";
+import Profile from "../Profile/Profile.js";
+import Login from "./Login.js";
+import Register from "./Register.js";
+
+function App() {
+  const history = useHistory();
+
+  return (
+    <div className="page">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">
+          <SavedMovies />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/signin">
+          <Login history={history} />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
