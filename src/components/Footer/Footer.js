f78a7a4a10e9772e42baac20c468724399c32a05
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-10-21 09:17:26 +0300

    feat: style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Footer() {
-
-};
+  return (
+    <footer className='footer'>
+      <p className='footer__desc'>
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className='footer__bottom-pannel'>
+        <p className='footer__copyright'>&copy; 2022</p>
+        <ul className='footer__links'>
+          <li className='footer__item'>
+            <a className='footer__link' href='#'>
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li className='footer__item'>
+            <a className='footer__link' href='#'>
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import AboutProject from "./AboutProject/AboutProject.js";
 import Techs from "./Techs/Techs.js";
 import AboutMe from "./AboutMe/AboutMe.js";
 import Portfolio from "./Portfolio/Portfolio.js";
+import Footer from "../Footer/Footer.js";
 
 function Main() {
   return (
