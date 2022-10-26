06e7e00abd27c025fef9fe56601b6f898cb305bd
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-10-26 23:23:55 +0300

    feat: style the Profile page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
+import CurrentUserContext from "../../contexts/CurrentUserContext.js";
 import Main from "../Main/Main.js";
 import Movies from "../Movies/Movies.js";
 import SavedMovies from "../SavedMovies/SavedMovies.js";
@@ -11,30 +12,33 @@ import initialMovies from "../../utils/initialMovies.js";
 function App() {
   const history = useHistory();
   const [movies, setMovies] = useState(initialMovies);
+  const [currentUser, setCurrentUser] = useState({});
 
   return (
-    <div className='page'>
-      <Switch>
-        <Route exact path='/'>
-          <Main />
-        </Route>
-        <Route path='/movies'>
-          <Movies movies={movies} />
-        </Route>
-        <Route path='/saved-movies'>
-          <SavedMovies movies={movies} />
-        </Route>
-        <Route path='/profile'>
-          <Profile />
-        </Route>
-        <Route path='/signin'>
-          <Login history={history} />
-        </Route>
-        <Route path='/signup'>
-          <Register />
-        </Route>
-      </Switch>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className='page'>
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/movies'>
+            <Movies movies={movies} />
+          </Route>
+          <Route path='/saved-movies'>
+            <SavedMovies movies={movies} />
+          </Route>
+          <Route path='/profile'>
+            <Profile />
+          </Route>
+          <Route path='/signin'>
+            <Login history={history} />
+          </Route>
+          <Route path='/signup'>
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,42 @@
 import React from "react";
+import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
 
 function Profile() {
-
-};
+  return (
+    <main className='profile'>
+      <Header />
+      <h1 className='profile__header'>Привет, Виталий!</h1>
+      <form className='profile__form'>
+        <div className='profile__input-container profile__name'>
+          <p className='profile__input-name'>Имя</p>
+          <input
+            className='profile__input'
+            name='name'
+            placeholder='имя'
+            disabled
+            value='Виталий'
+          />
+        </div>
+        <div className='profile__input-container profile__email'>
+          <p className='profile__input-name'>E-mail</p>
+          <input
+            className='profile__input'
+            name='email'
+            placeholder='email'
+            disabled
+            value='pochta@yandex.ru'
+          />
+        </div>
+        <button type='button' className='profile__edit-button profile__button'>
+          Редактировать
+        </button>
+      </form>
+      <button type='button' className='profile__exit-button profile__button'>
+        Выйти из аккаунта
+      </button>
+    </main>
+  );
+}
 
 export default Profile;
