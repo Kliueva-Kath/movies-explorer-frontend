b28b72a83315b78e646aa3301c3e5f0e53e6f79c
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-10-29 23:55:11 +0300

    fix: Portfolio links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,46 +8,46 @@ function Portfolio() {
       <h2 className='portfolio__header'>Портфолио</h2>
       <ul className='portfolio__list'>
         <li className='portfolio__item'>
-          <p className='portfolio__site'>Статичный сайт</p>
           <a
             className='portfolio__link'
             href='https://kliueva-kath.github.io/how-to-learn/'
             target='_blank'
             rel='noopener noreferrer'>
-            <img
-              className='portfolio__arrow'
-              alt='Перейти на сайт'
-              src={arrowPath}
-            />
+            Статичный сайт
           </a>
+          <img
+            className='portfolio__arrow'
+            alt='Перейти на сайт'
+            src={arrowPath}
+          />
         </li>
         <li className='portfolio__item'>
-          <p className='portfolio__site'>Адаптивный сайт</p>
           <a
             className='portfolio__link'
             href='https://kliueva-kath.github.io/russian-travel/'
             target='_blank'
             rel='noopener noreferrer'>
-            <img
-              className='portfolio__arrow'
-              alt='Перейти на сайт'
-              src={arrowPath}
-            />
+            Адаптивный сайт
           </a>
+          <img
+            className='portfolio__arrow'
+            alt='Перейти на сайт'
+            src={arrowPath}
+          />
         </li>
         <li className='portfolio__item'>
-          <p className='portfolio__site'>Одностраничное приложение</p>
           <a
             className='portfolio__link'
             href='https://mesto.kliueva.nomoredomains.club/sign-in'
             target='_blank'
             rel='noopener noreferrer'>
-            <img
-              className='portfolio__arrow'
-              alt='Перейти на сайт'
-              src={arrowPath}
-            />
+            Одностраничное приложение
           </a>
+          <img
+            className='portfolio__arrow'
+            alt='Перейти на сайт'
+            src={arrowPath}
+          />
         </li>
       </ul>
     </section>
